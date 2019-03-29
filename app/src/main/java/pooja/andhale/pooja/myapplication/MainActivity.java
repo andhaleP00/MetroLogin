d96122cb2f853a4c178bf8858379ece70055355c
Pooja Andhale <poojaa@pune-dev-dt-006.payso.in>
2019-03-29 19:13:04 +0530

    minor corrections

--- app/src/main/java/pooja/andhale/pooja/myapplication/MainActivity.java
+++ app/src/main/java/pooja/andhale/pooja/myapplication/MainActivity.java
@@ -1,12 +1,17 @@
 package pooja.andhale.pooja.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
@@ -20,44 +25,85 @@ public class MainActivity extends AppCompatActivity {
     EditText metroIdPassword;
     @BindView(R.id.Login)
     Button Login;
+    @BindView(R.id.forgotPassword)
+    TextView forgotPasswordTextview;
+    @BindView(R.id.signup)
+    TextView signupTextview;
     Activity currentActivity=this;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        metroId.setOnClickListener(onClickListener);
-        metroIdPassword.setOnClickListener(onClickListener);
+        metroId.addTextChangedListener(textWatcher);
+        metroIdPassword.addTextChangedListener(textWatcher);
         Login.setOnClickListener(onClickListener);
+        forgotPasswordTextview.setOnClickListener(onClickListener);
+        signupTextview.setOnClickListener(onClickListener);
 
     }
+
+    private TextWatcher textWatcher=new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        private void hideSoftKeyboard(View v)
+        {
+            InputMethodManager imm =(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if(s==metroId.getEditableText())
+            {
+                if(s.length()==10)
+                {
+                    hideSoftKeyboard(metroId);
+                    metroIdPassword.requestFocus();
+                }
+            }
+            else if(s==metroIdPassword.getEditableText())
+            {
+                if(s.length()==6)
+                {
+                    hideSoftKeyboard(metroIdPassword);
+                }
+            }
+
+
+        }
+    };
 
     private View.OnClickListener onClickListener=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch(v.getId())
             {
-                case R.id.metroId:
-                    if(metroId.length()==10)
-                    {
-                        Log.d("login","clicked to metroId");
-                    }
-                    break;
-                case R.id.metroIdPassword:
-                    if(metroIdPassword.length()==6)
-                    {
-                        Log.d("login","enterd min 6");
-                    }
-                    break;
                 case R.id.Login:
                     if((metroId.length()==10)&&(metroIdPassword.length()>=6))
                     {
-                        Intent intent=new Intent(currentActivity,OTPActivity.class);
+                        intent=new Intent(currentActivity,OTPActivity.class);
                         startActivity(intent);
                     }
-
-
+                    break;
+                case R.id.forgotPassword:
+                    intent=new Intent(currentActivity,ForgotPasswordActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.signup:
+                    intent=new Intent(currentActivity,SignupActivity.class);
+                    startActivity(intent);
+                    break;
             }
 
 
--- app/src/main/java/pooja/andhale/pooja/myapplication/OTPActivity.java
+++ app/src/main/java/pooja/andhale/pooja/myapplication/OTPActivity.java
@@ -1,6 +1,8 @@
 package pooja.andhale.pooja.myapplication;
 
 import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -10,6 +12,8 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
@@ -33,21 +37,52 @@ public class OTPActivity extends AppCompatActivity implements View.OnFocusChange
      EditText pinFourthEditText;
     @BindView(R.id.pin_hidden_edittext)
     EditText pinHiddenEditText;
+    @BindView(R.id.resendOTP)
+    TextView resendOTPTextview;
     Integer textLength;
+    Intent intent;
+    String otp;
 
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+        if((start==4)&&(s.length()>4))
+        {
+            start=0;
+        }
     }
+    private TextWatcher textWatcher=new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if(s.length()==10)
+            {
+                InputMethodManager imm =(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(mobNumber.getWindowToken(), 0);
+            }
+
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
         ButterKnife.bind(this);
+        mobNumber.addTextChangedListener(textWatcher);
         getOtp.setOnClickListener(onClickListener);
         submit.setOnClickListener(onClickListener);
+        resendOTPTextview.setOnClickListener(onClickListener);
         setPINListeners();
 
 
@@ -175,7 +210,7 @@ public class OTPActivity extends AppCompatActivity implements View.OnFocusChange
 
     @Override
     public void onFocusChange(View v, boolean hasFocus) {
-        final int id = v.getId();
+        int id = v.getId();
         switch (id) {
             case R.id.pin_first_edittext:
                 if (hasFocus) {
@@ -244,32 +279,47 @@ public class OTPActivity extends AppCompatActivity implements View.OnFocusChange
 
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
-        if (s.length() == 0) {
-            setFocusedPinBackground(pinFirstEditText);
-            pinFirstEditText.setText("");
-        } else if (s.length() == 1) {
-            setFocusedPinBackground(pinSecondEditText);
-            pinFirstEditText.setText(s.charAt(0) + "");
-            pinSecondEditText.setText("");
-            pinThirdEditText.setText("");
-            pinFourthEditText.setText("");
-        } else if (s.length() == 2) {
-            setFocusedPinBackground(pinThirdEditText);
-            pinSecondEditText.setText(s.charAt(1) + "");
-            pinThirdEditText.setText("");
-            pinFourthEditText.setText("");
-        } else if (s.length() == 3) {
-            setFocusedPinBackground(pinFourthEditText);
-            pinThirdEditText.setText(s.charAt(2) + "");
-            pinFourthEditText.setText("");
-        } else if (s.length() == 4) {
-//            setFocusedPinBackground(pinFourthEditText);
-            pinFourthEditText.setText(s.charAt(3) + "");
+        if((start<=4)&&(s.length()<=4)) {
+            if (s.length() == 0) {
+                setFocusedPinBackground(pinFirstEditText);
+                pinFirstEditText.setText("");
+            } else if (s.length() == 1) {
+                setFocusedPinBackground(pinSecondEditText);
+                pinFirstEditText.setText(s.charAt(0) + "");
+                pinSecondEditText.setText("");
+                pinThirdEditText.setText("");
+                pinFourthEditText.setText("");
+            } else if (s.length() == 2) {
+                setFocusedPinBackground(pinThirdEditText);
+                pinSecondEditText.setText(s.charAt(1) + "");
+                pinThirdEditText.setText("");
+                pinFourthEditText.setText("");
+            } else if (s.length() == 3) {
+                setFocusedPinBackground(pinFourthEditText);
+                pinThirdEditText.setText(s.charAt(2) + "");
+                pinFourthEditText.setText("");
+            } else if (s.length() == 4) {
+                setFocusedPinBackground(pinFirstEditText);
+                pinFourthEditText.setText(s.charAt(3) + "");
+                otp=s.toString();
+            }
+        }
+        else
+        {
+//            s="";
+          Toast.makeText(getApplicationContext(),"onTextChanged() with start="+start+".",Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     public void afterTextChanged(Editable s) {
+        if(s.length()>4)
+        {
+            Character s1=s.charAt(4);
+            s.clear();
+            s.append(s1);
+        }
+
 
     }
 
@@ -311,11 +361,33 @@ public class OTPActivity extends AppCompatActivity implements View.OnFocusChange
                     if(mobNumber.length()==10)
                     {
                         //above will check for valid mobile
+                        pinEditTextVisibility();
+                        Toast.makeText(getApplicationContext(),"call sendOTP api",Toast.LENGTH_SHORT).show();
+
                     }
+                    break;
+                case R.id.resendOTP:
+                   pinEditTextVisibility();
+                   Toast.makeText(getApplicationContext(),"call sendOTP api",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.submit:
+                    Toast.makeText(getApplicationContext(),"otp="+otp+".",Toast.LENGTH_LONG).show();
+                    break;
+
             }
 
         }
     };
+
+    public void pinEditTextVisibility()
+    {
+        pinFirstEditText.setText("");
+        pinSecondEditText.setText("");
+        pinThirdEditText.setText("");
+        pinFourthEditText.setText("");
+//        onFocusChange(pinFirstEditText,true);
+
+    }
     public void showSoftKeyboard(EditText editText) {
         if (editText == null)
             return;
